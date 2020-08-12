935d19dc0d62c8c8221be2cdb6ce981cc2146905
KiryaRizhiy <kilybox@gmail.com>
2020-08-12 19:33:04 +0300

    Car select interface update
    And minor changes

--- Traffic Way/Assets/Scripts/Object/CarSelectInterface.cs
+++ Traffic Way/Assets/Scripts/Object/CarSelectInterface.cs
@@ -7,74 +7,108 @@ public class CarSelectInterface : MonoBehaviour
 {
     private static Texture2D navigationDotActive;
     private static Texture2D navigationDotInactive;
-    private float navigationDotsDistance = 10f;
-    private float navigationDotsSize = 5f;
+    private static Texture2D regularCarBackground;
+    private static Texture2D selectedCarBackground;
+    private float navigationDotsDistance = 12f;
+    private float navigationDotsSize = 6f;
     private static int tappedPanelNum;
     private static int _currentPage;
+    private const int _carsOnPage = 4;
 
     private Transform _navigationPanel
     {
-        get { return transform.GetChild(0).GetChild(0).GetChild(3); }
+        get { return transform.GetChild(1).GetChild(0).GetChild(3); }
     }
     private Transform _turnLeftButton
     {
         get
         {
-            return transform.GetChild(0).GetChild(0).GetChild(0);
+            return transform.GetChild(1).GetChild(0).GetChild(0);
         }
     }
     private Transform _turnRightButton
     {
         get
         {
-            return transform.GetChild(0).GetChild(0).GetChild(1);
+            return transform.GetChild(1).GetChild(0).GetChild(1);
         }
     }
     private Transform _carAppearencesPanel
     {
         get
         {
-            return transform.GetChild(0).GetChild(1);
+            return transform.GetChild(1).GetChild(1);
+        }
+    }
+    //private Transform _carUnlockConfirmationPanel
+    //{
+    //    get
+    //    {
+    //        return transform.GetChild(2);
+    //    }
+    //}
+    private Transform _carUnlockedPanel
+    {
+        get
+        {
+            return transform.GetChild(2);
+        }
+    }
+    private Transform _carSelectPanel
+    {
+        get
+        {
+            return transform.GetChild(1);
         }
     }
     private int _pagesAmt
     {
         get 
         {
-            if (Engine.CarsAppearences.Count % 6 > 0)
-                return Engine.CarsAppearences.Count / 6 + 1;
+            if (Engine.CarsAppearencesAngled.Count % _carsOnPage > 0)
+                return Engine.CarsAppearencesAngled.Count / _carsOnPage + 1;
             else
-                return Engine.CarsAppearences.Count / 6;
+                return Engine.CarsAppearencesAngled.Count / _carsOnPage;
         }
     }
 
     public static void LoadResources()
     {
-        navigationDotActive = Resources.Load<Texture2D>("TrafficWay/Textures/Interface/NavDotActive");
-        navigationDotInactive = Resources.Load<Texture2D>("TrafficWay/Textures/Interface/NavDotInactive");
+        navigationDotActive = Resources.Load<Texture2D>("TrafficWay/Textures/Interface/ChooseCar/choose_dot_1");
+        navigationDotInactive = Resources.Load<Texture2D>("TrafficWay/Textures/Interface/ChooseCar/choose_dot_2");
+        regularCarBackground = Resources.Load<Texture2D>("TrafficWay/Textures/Interface/ChooseCar/unpick_car_bg");
+        selectedCarBackground = Resources.Load<Texture2D>("TrafficWay/Textures/Interface/ChooseCar/pick_car_bg");
     }
     public static void UnlockAppearence()
     {
-        Engine.meta.car.UnlockAppearence(_currentPage * 6 + tappedPanelNum);
+        Engine.meta.car.UnlockAppearence(_currentPage * _carsOnPage + tappedPanelNum);
     }
 
     public void SwipeRight()
     {
         _currentPage++;
         ShowPage();
+        Debug.Log("Swiped right");
     }
     public void SwipeLeft()
     {
         _currentPage--;
         ShowPage();
+        Debug.Log("Swiped left");
     }
     public void Open()
     {
+        _carSelectPanel.gameObject.SetActive(true);
+        //_carUnlockConfirmationPanel.gameObject.SetActive(false);
+        _carUnlockedPanel.gameObject.SetActive(false);
         _currentPage = 0;
         ShowPage();
     }
     public void Refresh()
     {
+        _carSelectPanel.gameObject.SetActive(true);
+        //_carUnlockConfirmationPanel.gameObject.SetActive(false);
+        _carUnlockedPanel.gameObject.SetActive(false);
         ShowPage();
     }
     public void Close()
@@ -85,10 +119,10 @@ public class CarSelectInterface : MonoBehaviour
     public void AppearenceTapped(int panelNum)
     {
         tappedPanelNum = panelNum;
-        switch (Engine.GetAppearenceState(_currentPage * 6 + panelNum))
+        switch (Engine.GetAppearenceState(_currentPage * _carsOnPage + panelNum))
         {
             case CarAppearenceState.Unlocked:
-                Engine.meta.car.SwitchCurrentAppearenceTo(_currentPage * 6 + panelNum);
+                Engine.meta.car.SwitchCurrentAppearenceTo(_currentPage * _carsOnPage + panelNum);
                 SwitchContent();
                 break;
             case CarAppearenceState.Locked:
@@ -96,22 +130,26 @@ public class CarSelectInterface : MonoBehaviour
                 break;
             case CarAppearenceState.Passed:
                 Debug.Log("It is passed");
-                transform.GetChild(0).GetChild(2).gameObject.SetActive(true);
-                transform.GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetComponent<Image>().sprite = Sprite.Create(
-                    Engine.CarsAppearences[_currentPage * 6 + panelNum],
-                    new Rect(0f, 0f, Engine.CarsAppearences[_currentPage * 6 + panelNum].width, Engine.CarsAppearences[_currentPage * 6 + panelNum].height),
+                if (!AdMobController.isRewardedVideoReady)
+                    return;
+                transform.parent.parent.gameObject.GetComponent<UserInteraction>().ShowCarAds();
+                //_carUnlockConfirmationPanel.gameObject.SetActive(true);
+                //_carUnlockConfirmationPanel.GetChild(0).GetComponent<Image>().sprite = Sprite.Create(
+                //    Engine.CarsAppearences[_currentPage * _carsOnPage + panelNum],
+                //    new Rect(0f, 0f, Engine.CarsAppearences[_currentPage * _carsOnPage + panelNum].width, Engine.CarsAppearences[_currentPage * _carsOnPage + panelNum].height),
+                //    Vector2.one * 0.5f);
+                _carUnlockedPanel.GetChild(0).GetComponent<Image>().sprite = Sprite.Create(
+                    Engine.CarsAppearences[_currentPage * _carsOnPage + panelNum],
+                    new Rect(0f, 0f, Engine.CarsAppearences[_currentPage * _carsOnPage + panelNum].width, Engine.CarsAppearences[_currentPage * _carsOnPage + panelNum].height),
                     Vector2.one * 0.5f);
-                transform.GetChild(0).GetChild(3).GetChild(0).GetChild(0).GetComponent<Image>().sprite = Sprite.Create(
-                    Engine.CarsAppearences[_currentPage * 6 + panelNum],
-                    new Rect(0f, 0f, Engine.CarsAppearences[_currentPage * 6 + panelNum].width, Engine.CarsAppearences[_currentPage * 6 + panelNum].height),
-                    Vector2.one * 0.5f);
-                if (AdMobController.isRewardedVideoReady)
-                    transform.GetChild(0).GetChild(2).GetChild(1).GetComponent<Button>().interactable = true;
-                else
-                    Engine.Events.adLoaded += ActivateButtonAndUnsubscribe;
+                //if (AdMobController.isRewardedVideoReady)
+                //    _carUnlockConfirmationPanel.GetChild(1).GetComponent<Button>().interactable = true;
+                //else
+                //    Engine.Events.adLoaded += ActivateButtonAndUnsubscribe;
+                //_carSelectPanel.gameObject.SetActive(false);
                 break;
             default:
-                Debug.Log("Unknown appearence " + (_currentPage * 6 + panelNum).ToString() + " state " + Engine.GetAppearenceState(_currentPage * 6 + panelNum).ToString());
+                Debug.Log("Unknown appearence " + (_currentPage * _carsOnPage + panelNum).ToString() + " state " + Engine.GetAppearenceState(_currentPage * _carsOnPage + panelNum).ToString());
                 break;
         }
     }
@@ -181,11 +219,11 @@ public class CarSelectInterface : MonoBehaviour
     }
     private void SwitchContent()
     {
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < _carsOnPage; i++)
         {
             _carAppearencesPanel.GetChild(i).GetComponent<Image>().color = Color.white;
-            Debug.Log("Car " + (_currentPage * 6 + i) + " is " + Engine.GetAppearenceState(_currentPage * 6 + i).ToString());
-            switch (Engine.GetAppearenceState(_currentPage * 6 + i))
+            Debug.Log("Car " + (_currentPage * _carsOnPage + i) + " is " + Engine.GetAppearenceState(_currentPage * _carsOnPage + i).ToString());
+            switch (Engine.GetAppearenceState(_currentPage * _carsOnPage + i))
             {
                 case CarAppearenceState.Missing:
                     _carAppearencesPanel.GetChild(i).gameObject.SetActive(false);
@@ -193,36 +231,47 @@ public class CarSelectInterface : MonoBehaviour
                 case CarAppearenceState.Locked:
                     _carAppearencesPanel.GetChild(i).gameObject.SetActive(true);
                     _carAppearencesPanel.GetChild(i).GetChild(0).GetComponent<Image>().sprite = Sprite.Create(
-                        Engine.CarAppearencesShadows[_currentPage * 6 + i]
-                        , new Rect(0f, 0f, Engine.CarAppearencesShadows[_currentPage * 6 + i].width, Engine.CarAppearencesShadows[_currentPage * 6 + i].height)
+                        Engine.CarAppearencesShadowsAngled[_currentPage * _carsOnPage + i]
+                        , new Rect(0f, 0f, Engine.CarAppearencesShadowsAngled[_currentPage * _carsOnPage + i].width, Engine.CarAppearencesShadowsAngled[_currentPage * _carsOnPage + i].height)
                         , Vector2.one * 0.5f);
-                    _carAppearencesPanel.GetChild(i).GetChild(1).gameObject.SetActive(true);
-                    _carAppearencesPanel.GetChild(i).GetChild(1).GetComponent<Text>().text = Localization.GetLocal("LevelToOpenACar") + ComputeLevelForAppearence(_currentPage * 6 + i).ToString();
-                    _carAppearencesPanel.GetChild(i).GetChild(2).gameObject.SetActive(true);
+                    _carAppearencesPanel.GetChild(i).GetChild(1).gameObject.SetActive(false);
+                    _carAppearencesPanel.GetChild(i).GetComponent<Image>().sprite = Sprite.Create(
+                        regularCarBackground
+                        , new Rect(0f, 0f, regularCarBackground.width, regularCarBackground.height)
+                        , Vector2.one * 0.5f);
                     break;
                 case CarAppearenceState.Unlocked:
                     _carAppearencesPanel.GetChild(i).gameObject.SetActive(true);
                     _carAppearencesPanel.GetChild(i).GetChild(0).GetComponent<Image>().sprite = Sprite.Create(
-                        Engine.CarsAppearences[_currentPage * 6 + i]
-                        , new Rect(0f, 0f, Engine.CarsAppearences[_currentPage * 6 + i].width, Engine.CarsAppearences[_currentPage * 6 + i].height)
+                        Engine.CarsAppearencesAngled[_currentPage * _carsOnPage + i]
+                        , new Rect(0f, 0f, Engine.CarsAppearencesAngled[_currentPage * _carsOnPage + i].width, Engine.CarsAppearencesAngled[_currentPage * _carsOnPage + i].height)
                         , Vector2.one * 0.5f);
                     _carAppearencesPanel.GetChild(i).GetChild(1).gameObject.SetActive(false);
-                    _carAppearencesPanel.GetChild(i).GetChild(2).gameObject.SetActive(false);
-                    if (Engine.meta.car.currentAppearenceNum == _currentPage * 6 + i)
-                        _carAppearencesPanel.GetChild(i).GetComponent<Image>().color = Color.yellow;
+                    if (Engine.meta.car.currentAppearenceNum == _currentPage * _carsOnPage + i)
+                        _carAppearencesPanel.GetChild(i).GetComponent<Image>().sprite = Sprite.Create(
+                            selectedCarBackground
+                            , new Rect(0f, 0f, selectedCarBackground.width, selectedCarBackground.height)
+                            , Vector2.one * 0.5f);
+                    else
+                        _carAppearencesPanel.GetChild(i).GetComponent<Image>().sprite = Sprite.Create(
+                            regularCarBackground
+                            , new Rect(0f, 0f, regularCarBackground.width, regularCarBackground.height)
+                            , Vector2.one * 0.5f);
                     break;
                 case CarAppearenceState.Passed:
                     _carAppearencesPanel.GetChild(i).gameObject.SetActive(true);
                     _carAppearencesPanel.GetChild(i).GetChild(0).GetComponent<Image>().sprite = Sprite.Create(
-                        Engine.CarAppearencesShadows[_currentPage * 6 + i]
-                        , new Rect(0f, 0f, Engine.CarAppearencesShadows[_currentPage * 6 + i].width, Engine.CarAppearencesShadows[_currentPage * 6 + i].height)
+                        Engine.CarAppearencesShadowsAngled[_currentPage * _carsOnPage + i]
+                        , new Rect(0f, 0f, Engine.CarAppearencesShadowsAngled[_currentPage * _carsOnPage + i].width, Engine.CarAppearencesShadowsAngled[_currentPage * _carsOnPage + i].height)
                         , Vector2.one * 0.5f);
                     _carAppearencesPanel.GetChild(i).GetChild(1).gameObject.SetActive(true);
-                    _carAppearencesPanel.GetChild(i).GetChild(1).GetComponent<Text>().text = Localization.GetLocal("WatchVideoToOpenACar");
-                    _carAppearencesPanel.GetChild(i).GetChild(2).gameObject.SetActive(false);
+                    _carAppearencesPanel.GetChild(i).GetComponent<Image>().sprite = Sprite.Create(
+                        regularCarBackground
+                        , new Rect(0f, 0f, regularCarBackground.width, regularCarBackground.height)
+                        , Vector2.one * 0.5f);
                     break;
                 default:
-                    Debug.LogError("Unknown car appearence type: " + Engine.GetAppearenceState(_currentPage * 6 + i));
+                    Debug.LogError("Unknown car appearence type: " + Engine.GetAppearenceState(_currentPage * _carsOnPage + i));
                     break;
             }
         }
