9e0a1c4948dec24b407be430dd42d269ae65aeff
KiryaRizhiy <51493042+KiryaRizhiy@users.noreply.github.com>
2020-11-05 21:06:23 +0300

    Puzzle content

--- Traffic Way/Assets/Scripts/Object/LevelGenerator.cs
+++ Traffic Way/Assets/Scripts/Object/LevelGenerator.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System;
 using GameAnalyticsSDK;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelGenerator : MonoBehaviour
 {
     private static GameObject[] Crosses;
     private static GameObject[] Situations;
     private static GameObject[] Bosses;
+    private static GameObject[] Puzzles;
     private static GameObject FinishBlock;
     private static GameObject FourCoins;
     private static GameObject FourCarsBeforeBoss;
@@ -20,6 +22,7 @@ public class LevelGenerator : MonoBehaviour
         Crosses = Resources.LoadAll<GameObject>("TrafficWay/Prefabs/Blocks/Crosses");
         Situations = Resources.LoadAll<GameObject>("TrafficWay/Prefabs/Blocks/Situations");
         Bosses = Resources.LoadAll<GameObject>("TrafficWay/Prefabs/Blocks/Bosses");
+        Puzzles = Resources.LoadAll<GameObject>("TrafficWay/Prefabs/Blocks/Puzzles");
         FinishBlock = Resources.Load<GameObject>("TrafficWay/Prefabs/Blocks/Special/FinishRoad");
         FourCoins = Resources.Load<GameObject>("TrafficWay/Prefabs/Blocks/Special/FourCoins");
         FourCarsBeforeBoss = Resources.Load<GameObject>("TrafficWay/Prefabs/Blocks/Special/FourCarsBeforeBoss");
@@ -28,6 +31,11 @@ public class LevelGenerator : MonoBehaviour
     }
 
     public static bool isBossFignt
+    {
+        get;
+        private set;
+    }
+    public static bool isPuzzle
     {
         get;
         private set;
@@ -81,11 +89,17 @@ public class LevelGenerator : MonoBehaviour
                 isBossFignt = true;
                 break;
             case 5:
-                if (UnityEngine.Random.Range(0, 99) <= 49)
+                float _r = UnityEngine.Random.Range(0, 99);
+                if (_r <= 49)
                 {
                     BlockSequence.Add(Crosses[UnityEngine.Random.Range(0, Crosses.Length)].name);
                     BlockSequence.Add(FourCoins.name);
                     BlockSequence.Add(Situations[UnityEngine.Random.Range(0, Situations.Length)].name);
+                }
+                else
+                if(_r>= 50 && _r <= 79)
+                {
+                    BlockSequence.Add(Puzzles[UnityEngine.Random.Range(0, Puzzles.Length)].name);
                 }
                 else
                 {
@@ -124,9 +138,17 @@ public class LevelGenerator : MonoBehaviour
                 BlockSequence.Add(FourCoins.name);
                 break;
             case 0:
-                BlockSequence.Add(FourCoins.name);
-                BlockSequence.Add(FourCoins.name);
-                BlockSequence.Add(FourCoins.name);
+
+                if (UnityEngine.Random.Range(0, 99) <= 69)
+                {
+                    BlockSequence.Add(FourCoins.name);
+                    BlockSequence.Add(FourCoins.name);
+                    BlockSequence.Add(FourCoins.name);
+                }
+                else
+                {
+                    BlockSequence.Add(Puzzles[UnityEngine.Random.Range(0, Puzzles.Length)].name);
+                }
                 break;
             default:
                 Debug.LogError("Unknown lvl type " + lvlType);
@@ -149,6 +171,8 @@ public class LevelGenerator : MonoBehaviour
         //Build sequence
         int lvlLength = 0;
         GameObject _currB;
+        isBossFignt = false;
+        isPuzzle = false;
         foreach (string _b in Engine.meta.currentRandomLevelBlocks)
         {
             GameObject _prefab;
@@ -173,23 +197,38 @@ public class LevelGenerator : MonoBehaviour
                                 if (FourCarsBeforeBoss.name == _b)
                                     _prefab = FourCarsBeforeBoss;
                                 else
-                                {
-                                    Debug.LogError("Unknown level block type " + _b + ". Used Finish block instead");
-                                    GameAnalytics.NewErrorEvent(GAErrorSeverity.Warning, "Unknown level block type " + _b + ". Used Finish block instead");
-                                    _prefab = FinishBlock;
-                                }
-            _currB = Instantiate(_prefab, transform);
+                                    if(Array.Find<GameObject>(Puzzles, x => x.name == _b) != null)
+                                    {
+                                        isPuzzle = true;
+                                        _prefab = Array.Find<GameObject>(Puzzles, x => x.name == _b);
+                                       }
+                                    else
+                                    { 
+                                        Debug.LogError("Unknown level block type " + _b + ". Used Finish block instead");
+                                        GameAnalytics.NewErrorEvent(GAErrorSeverity.Warning, "Unknown level block type " + _b + ". Used Finish block instead");
+                                        _prefab = FinishBlock;
+                                    }
+            
+            _currB = Instantiate(_prefab, transform); 
+            if (!isPuzzle)
+            {
+                _currB.GetComponent<Block>().environmentType = Engine.meta.environmentType;
+                _currB.GetComponent<Block>().Show();
+                _currB.transform.position = new Vector3(0f, lvlLength, 0f);
+                lvlLength += _prefab.GetComponent<Block>().length;
+            }
+            _currB.name = _b;
+        }
+        if (isPuzzle)
+            SceneManager.GetActiveScene().GetRootGameObjects()[2].SetActive(false);        
+        else
+        {
+            _currB = Instantiate(StartBlock, transform);
             _currB.GetComponent<Block>().environmentType = Engine.meta.environmentType;
             _currB.GetComponent<Block>().Show();
-            _currB.transform.position = new Vector3(0f, lvlLength, 0f);
-            _currB.name = _b;
-            lvlLength += _prefab.GetComponent<Block>().length;
+            _currB.transform.position = new Vector3(0f, -37f, 0f);
+            _currB.name = "Start block";
         }
-        _currB = Instantiate(StartBlock, transform);
-        _currB.GetComponent<Block>().environmentType = Engine.meta.environmentType;
-        _currB.GetComponent<Block>().Show();
-        _currB.transform.position = new Vector3(0f, -37f, 0f);
-        _currB.name = "Start block";
         //If boss fight
         if (isBossFignt)
             CarDriver.CurrentCar.GetComponent<CarShooter>().enabled = true;
